70961976ab8a2fbdff9168497ee8b25edf00b750
Phuc1912001 <phucphuc1912001@gmail.com>
2022-11-22 19:22:38 +0700

    orderController , oderModel , orderRoute

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,11 @@ app.use(cookieParser());
 //Route imports
 const product = require("./routes/ProductRoute");
 const user = require("./routes/UserRoute")
+const order = require("./routes/OrderRoute");
 
 app.use("/api/v2", product);
 app.use("/api/v2", user);
+app.use("/api/v2", order);
 
 
 app.use(ErrorHandler);
